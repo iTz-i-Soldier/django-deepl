014e998eddbc2dee096bc2e33d626709928a3aa0
iTz-i-Soldier <81917169+iTz-i-Soldier@users.noreply.github.com>
2025-03-09 22:35:52 +0100

    Initial release to PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="django-deepl",
-    version="0.1.0",
+    version="0.0.1",
     description="Django app to translate .po files using DeepL translation service",
     author="iTz-i-Soldier",
     author_email="no",
     url="https://github.com/iTz-i-Soldier/django-deepl",  
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
